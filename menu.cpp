0d653a9e43cda4a39374d94a27673d673d4f3042
kyler <100742516+ky-ler@users.noreply.github.com>
2023-05-06 07:57:07 -0400

    Fix auto dialogue speed

--- menu.cpp
+++ menu.cpp
@@ -29,7 +29,8 @@ namespace menu {
 
 	void set_speed(float speed) {
 		if (auto_dialogue) {
-			set_speed_battle(5.f);
+			set_speed_battle(2.f);
+			set_speed_global(2.f);
 		}
 		else if (hooks::game::current_phase == RPG_BATTLE) {
 			set_speed_battle(speed * 5);
